d69a33948269fd7e6c805d8db15414cd8164370f
github username <github email address>
2020-06-11 15:47:56 -0600

    set up basic actions in user controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
 
   namespace :api do 
     namespace :v1 do 
-      resources :users
+      resources :users 
     end
   end
-  resources :destinations
+  resources :destinations  #! namespace this and nest it in users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
