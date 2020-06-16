53591b080f2470818ede7caa3acce07409c5e210
github username <github email address>
2020-06-16 14:18:02 -0600

    fill in origins with * in rack cors, fixed syntax error in application_controller helpers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,16 @@
 class ApplicationController < ActionController::API
+
+  helpers do
+
+    def current_user 
+      #! mock version of being logged in. Change this.
+      User.first
+    end 
+
+    def logged_in? 
+      !!current_user
+    end
+
+  end
+  
 end
---
+++ app/notes.rb
@@ -0,0 +1,6 @@
+# need to fill in serializers for destination and cdc_info
+
+#decide whether destination should be a nested route under a user
+
+# currently fix mock login and helpers in application controller
+
