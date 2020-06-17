b1d517db3b40771d3a279e8c787848299c032e77
github username <github email address>
2020-06-17 14:54:12 -0600

    added additional user seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-User.create(username: 'demo user', password: 'password')
+User.create(username: 'demo user 1', password: 'password1')
+User.create(username: 'demo user 2', password: 'password2')
 Destination.create(name: 'paradise', user_id: 1, notes: 'such notes')
 CdcInfo.create(destination_id: 1, advisories: "beware", vaccinations: "get them, please")
