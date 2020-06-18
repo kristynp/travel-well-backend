8523eb624fb1c338e99e809ae4cf5212c8de3265
github username <github email address>
2020-06-18 13:43:23 -0600

    filled in create action in sessions controller. uncommented helpers in application controller

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::SessionsController < ApplicationController
   def create 
-    byebug
+    @user = User.find_by(username: params[:session][:username])
+
+    if @user && @user.authenticate(params[:session][:password])
+      session[:user_id] = @user.id
+      render json: @user
+    else
+      render json: {
+        error: "Invalid Credentials"
+      }
+    end
   end
 
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,13 @@
 class ApplicationController < ActionController::API
 
 
-  # def current_user 
-  #   #! mock version of being logged in. Change this.
-  #   User.first
-  # end 
+  def current_user 
+    User.find(session[:user_id])
+  end 
 
-  # def logged_in? 
-  #   !!current_user
-  # end
+  def logged_in? 
+    !!current_user
+  end
 
   # helper_method :current_user, :logged_in?
 
