03e5af157537c1af557b7a6a7c0b3f5cac380f6d
github username <github email address>
2020-06-22 17:57:30 -0600

    added signup route, and create functionality in users controller. Added status to login

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,6 @@ class Api::V1::UsersController < ApplicationController
 
   def index 
     @users = User.all
-
     render json: UserSerializer.new(@users, {include: [:destinations]}).serialized_json
   end
 
@@ -15,9 +14,11 @@ class Api::V1::UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      render json: @user, status: :created, location: @user
+      session[:user_id] = @user.id
+      render json: UserSerializer.new(@user), status: :created
     else 
-      render json: @user.errors, status: :unprocessable_entity
+      resp = {error: @user.errors.full_messages.to_sentence}
+      render json: resp, status: :unprocessable_entity
     end
   end
 
@@ -42,6 +43,6 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def user_params
-    params.require(:user).permit(:username, :password_digest)
+    params.require(:user).permit(:username, :password)
   end
 end
