64773e5a57e6ae431a270057922235ebde0c78fd
github username <kristyparker1@gmail.com>
2020-07-06 10:14:06 -0600

    added images to destinations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,5 +1,6 @@
 class Destination < ApplicationRecord
   belongs_to :user
+  has_many :images
   has_one :cdc_info
   validates :name, :user_id, presence: true
 end
--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -1,5 +1,6 @@
 class DestinationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :country, :user_id, :notes, :cdc_info
+  has_many :images, serializer: ImageSerializer
 end
 
