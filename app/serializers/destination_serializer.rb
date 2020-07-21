db73886ee43f6342b87db005077290a9f232bf57
github username <kristyparker1@gmail.com>
2020-07-21 17:49:11 -0600

    add comments to destination serializer

--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -1,6 +1,6 @@
 class DestinationSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :country, :user_id, :notes, :images
+  attributes :name, :country, :user_id, :notes, :images, :comments
   has_many :images, serializer: ImageSerializer
 end
 
