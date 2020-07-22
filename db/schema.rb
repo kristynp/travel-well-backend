cf911e1d7ea6a88ca6c8f0e919dd8a2c6ad5e51b
github username <kristyparker1@gmail.com>
2020-07-21 18:40:03 -0600

    add destination_id column to comments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_15_194524) do
+ActiveRecord::Schema.define(version: 2020_07_22_003437) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "destination_id"
+  end
 
   create_table "destinations", force: :cascade do |t|
     t.string "name"
