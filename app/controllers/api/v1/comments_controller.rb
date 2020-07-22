a9138bb0b53071974159cf8bc78d5e956f35dd5b
github username <kristyparker1@gmail.com>
2020-07-21 18:38:01 -0600

    remove index

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::CommentsController < ApplicationController
+  before_action :set_comment, only: [:show, :update, :destroy]
 
+  def index 
+    if logged_in?
+      @destinations = current_user.destinations
+      render json: DestinationSerializer.new(@destinations)
+    else
+      render json: {
+        error: "You must be logged in to view your destinations"
+      }
+    end
+  end
+
+  private
+
+  def set_comment 
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:title, :content)
+  end
 end
