9b765ff7a96c36e163f27796cc357abb6992f63c
github username <kristyparker1@gmail.com>
2020-07-21 18:39:38 -0600

    add has_many relationship for showing comments in serializer

--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -2,5 +2,6 @@ class DestinationSerializer
   include FastJsonapi::ObjectSerializer
   attributes :name, :country, :user_id, :notes, :images, :comments
   has_many :images, serializer: ImageSerializer
+  has_many :images, serializer: CommentSerializer
 end
 
