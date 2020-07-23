9cde32aae372a6536d9f85dcdef2fba7d5fc454c
github username <kristyparker1@gmail.com>
2020-07-22 19:00:12 -0600

    add comment attribute

--- app/serializers/destination_serializer.rb
+++ app/serializers/destination_serializer.rb
@@ -1,12 +1,7 @@
 class DestinationSerializer
   include FastJsonapi::ObjectSerializer
-<<<<<<< HEAD
-  attributes :name, :country, :user_id, :notes, :images
-  has_many :images, serializer: ImageSerializer, :comments, CommentSerializer
-=======
   attributes :name, :country, :user_id, :notes, :images, :comments
   has_many :images, serializer: ImageSerializer
   has_many :images, serializer: CommentSerializer
->>>>>>> comment
 end
 
