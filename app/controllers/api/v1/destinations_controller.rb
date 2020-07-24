e1c30cb12fcf031e1601c4bebea27b1569aaa389
github username <kristyparker1@gmail.com>
2020-07-24 11:23:40 -0600

    change status to ok when destination is created properly

--- app/controllers/api/v1/destinations_controller.rb
+++ app/controllers/api/v1/destinations_controller.rb
@@ -19,7 +19,7 @@ class Api::V1::DestinationsController < ApplicationController
   def create
     @destination = current_user.destinations.build(destination_params)
     if @destination.save
-      render json: DestinationSerializer.new(@destination), status: :created
+      render json: DestinationSerializer.new(@destination), status: :ok
     else
       error_message = @destination.errors.full_messages.to_sentence
       render json: error_message, status: :unprocessable_entity
